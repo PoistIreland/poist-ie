b07c34286263278a01004a2fa6b242d42b947666
Your Name <seamusofatharta@kinia.ie>
2026-04-18 23:00:40 +0100

    Fix logo - transparent PNG, remove blend mode

--- rebrand.py
+++ rebrand.py
@@ -6,7 +6,7 @@ from pathlib import Path
 LOGO_LIGHT = "assets/poist-logo-trans.png"
 LOGO_DARK  = "assets/poist-logo-trans.png"
 
-NAV_NEW  = f'<a href="index.html" class="nav-logo"><img src="{LOGO_LIGHT}" alt="poist.ie" style="height:48px;width:auto;display:block;mix-blend-mode:screen;"></a>'
+NAV_NEW  = f'<a href="index.html" class="nav-logo"><img src="{LOGO_LIGHT}" alt="poist.ie" style="height:48px;width:auto;display:block;"></a>'
 FOOT_NEW = f'<div class="foot-logo"><img src="{LOGO_DARK}" alt="poist.ie" style="height:38px;width:auto;filter:brightness(0) invert(1);"></div>'
 
 SKIP = {"coming-soon.html", "404.html"}
